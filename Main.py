d1d0c66a5f33f256bdec64310f7c79644ee3af79
James Peralta <jamesperalta35@gmail.com>
2019-07-27 18:24:15 -0600

    Add some API calls

--- Main.py
+++ Main.py
@@ -2,17 +2,42 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use a service account
+# Use a service account to connect to the databse
 cred = credentials.Certificate('./serviceAccount.json')
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-doc_ref = db.collection('users').document('Next')
-doc_ref.set({
-    'first': 'Ada',
-    'last': 'Lovelfsdace',
-    'born': 1815
-})
 
+"""
+Retrieve all Golds Gyms
+"""
+users_ref = db.collection(u'gyms').where(u'Name', u'==', "Golds")
+docs = users_ref.stream()
 
+print("Retrieve all Golds Gyms")
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+"""
+Retrieve all Golds Gyms in the SW
+"""
+users_ref = db.collection(u'gyms').where(u'Name', u'==', "Golds").where(u'Location', u'==', "SW")
+docs = users_ref.stream()
+
+print("Retrieve all Golds Gyms in the SW")
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+"""
+Retrieve all Machines for the Golds Gyms in the SW
+"""
+users_ref = db.collection(u'gyms').where(u'Name', u'==', "Golds").where(u'Location', u'==', u"N").limit(1)
+gym_doc = next(users_ref.stream())
+
+print("Retrieve all machines in the SW Golds Gyms")
+print("GymId: {}".format(gym_doc.id))
+users_ref = db.collection(u'gyms').document(gym_doc.id).collection("Machines")
+machine_docs = users_ref.stream()
+for doc in machine_docs:
+    temp = doc.to_dict()
+    print("{}: {}".format(temp["Name"], temp["MachineId"]))
