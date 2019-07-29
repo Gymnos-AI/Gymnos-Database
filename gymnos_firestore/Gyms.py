1be152985db5da8995bd47c03177c1d3b784ae53
jamesperalta <jamesperalta35@gmail.com>
2019-07-29 14:39:43 -0600

    Create a Gym and Machines API

--- gymnos_firestore/Gyms.py
+++ gymnos_firestore/Gyms.py
@@ -7,16 +7,41 @@ cred = credentials.Certificate('./serviceAccount.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def get_gym(gym):
+
+def create_gym(gym_name, location):
+    """
+    This function will create a gym in the database
+
+    :return: gymid - AutoID of gym that you created
+    """
+    if get_gym(gym_name, location) < 1:
+        new_gym_ref = db.collection(u'gyms').document()
+        gym_id = new_gym_ref.id
+        doc_data = {
+            u'GymId': gym_id,
+            u'Name': gym_name,
+            u'Location': location
+        }
+        db.collection(u'gyms').document(gym_id).set(doc_data)
+
+        return gym_id
+    else:
+        return "This Gym has already been created"
+
+
+def get_gym(gym_name, location):
     """
     Retrieve all instances of a gym
     """
-    users_ref = db.collection(u'gyms').where(u'Name', u'==', gym)
-    docs = users_ref.stream()
+    users_ref = db.collection(u'gyms').where(u'Name', u'==', gym_name).where(u'Location', u'==', location)
+    docs = users_ref.get()
 
-    print("Retrieve all Golds Gyms")
+    gyms_retrieved = 0
     for doc in docs:
+        gyms_retrieved += 1
         print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+    return gyms_retrieved
 
 
 def get_gym_by_location(gym, location):
