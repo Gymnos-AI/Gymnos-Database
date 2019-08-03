256b214b93b77b35115108bc12e5f16efa155075
jamesperalta <jamesperalta35@gmail.com>
2019-08-03 12:07:13 -0600

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gymnosfirestoreapi',
-    version='0.2.1',
+    version='0.2.2',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     packages=['gymnos_firestore'],
     install_requires=['firebase-admin==2.17.0'],
