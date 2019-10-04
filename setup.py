80aa441bb06bbb597fa1680b0f6466803d15c638
Fernando Valera <fernaandovalera@gmail.com>
2019-10-03 19:15:10 -0600

    Updated matchbox-orm version

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.4.0',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     packages=['gymnos_firestore'],
-    install_requires=['firebase-admin==2.17.0', 'matchbox-orm==0.2.2'],
+    install_requires=['firebase-admin==2.17.0', 'matchbox-orm==0.2.4'],
 )
